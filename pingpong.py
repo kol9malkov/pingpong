254de35e186e7329bedc92c9066b5bab040ed1af
kol9malkov <47697063+kol9malkov@users.noreply.github.com>
2024-04-10 13:58:08 +0300

    Добавил класс GameSprite и изменил размеры окна

--- pingpong.py
+++ pingpong.py
@@ -1,7 +1,20 @@
 from pygame import *
+from pygame.sprite import _Group
+
+''' КЛАССЫ '''
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
 
 ''' НАСТРОЙКА ОКНА'''
-W, H = 600, 500
+W = 600
+H = 500
 window = display.set_mode((W, H))
 display.set_caption('Пинг Понг')
 back = (3, 252, 244)
